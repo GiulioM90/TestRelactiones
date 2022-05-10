719a2b12f82817da8bf171812ad3f9c8e579b9b2
Giulio Mollo <giuliomollo90@gmail.com>
2022-05-10 07:22:06 +0200

    added slick slider in desktop index

---
+++ README.md
@@ -0,0 +1,1 @@
+#Jquery in script.js load resposive html
--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 if($(window).width() < 652){
     $( "#where" ).load( "mobileindex.html" );
 }
@@ -5,5 +6,16 @@ if($(window).width() > 653 &&  $(window).width() < 991){
     $( "#where" ).load( "tabletindex.html" );
 }
 if($(window).width() >= 992 ){
-    $( "#where" ).load( "desktopindex.html" );
+    $( "#where" ).load( "desktopindex.html", function (){
+        $(document).ready(function($) {
+            $(".slick-carousel").slick({
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                speed: 1500,
+                fade: true,
+            });
+            })
+    });
+    
 }
