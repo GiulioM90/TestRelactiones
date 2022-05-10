db95866103305ccf0441c053692dd9c9d8b3b29b
Giulio Mollo <giuliomollo90@gmail.com>
2022-05-10 07:29:18 +0200

    added slick slider in tablet index

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
 
 if($(window).width() < 652){
-    $( "#where" ).load( "mobileindex.html" );
+    $( "#where" ).load( "./Views/mobileindex.html" );
 }
 if($(window).width() > 653 &&  $(window).width() < 991){
-    $( "#where" ).load( "tabletindex.html" );
+    $( "#where" ).load( "./Views/tabletindex.html", function (){
+        $(document).ready(function($) {
+            $(".slick-carousel").slick({
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                speed: 1500,
+                fade: true,
+            });
+            })
+    });
 }
 if($(window).width() >= 992 ){
-    $( "#where" ).load( "desktopindex.html", function (){
+    $( "#where" ).load( "./Views/desktopindex.html", function (){
         $(document).ready(function($) {
             $(".slick-carousel").slick({
                 arrows: false,
